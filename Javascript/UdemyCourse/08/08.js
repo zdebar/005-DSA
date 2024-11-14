9452a561b7d4638b9813885a0514688619c1b22b
zdebar <157598603+zdebar@users.noreply.github.com>
2024-11-14 14:01:57 +0100

    finished 09

--- Javascript/UdemyCourse/08/08.js
+++ Javascript/UdemyCourse/08/08.js
@@ -1,9 +1,9 @@
-// Update / add an element to the dictionary (JavaScript object)
+// Update / add an element to the dictionary (JavaScript object) ----- OK
 const myDict = { name: 'Edy', age: 26 };
 myDict['address'] = 'London';
 console.log(myDict);
 
-// Traverse through a dictionary
+// Traverse through a dictionary ----- OK
 function traverseDict(dict) {
     for (let key in dict) {
         console.log(key, dict[key]);
@@ -12,7 +12,7 @@ function traverseDict(dict) {
 
 traverseDict(myDict);
 
-// Searching a dictionary
+// Searching a dictionary ----- OK
 function searchDict(dict, value) {
     for (let key in dict) {
         if (dict[key] === value) {
@@ -24,11 +24,11 @@ function searchDict(dict, value) {
 
 console.log(searchDict(myDict, 27));
 
-// Delete or remove a dictionary entry
+// Delete or remove a dictionary entry ----- OK
 delete myDict['name'];
 console.log(myDict);
 
-// Sorted method by key length
+// Sorted method by key length ----- OK
 const anotherDict = { eooooa: 1, aas: 2, udd: 3, sseo: 4, werwi: 5 };
 const sortedKeys = Object.keys(anotherDict).sort((a, b) => a.length - b.length);
 console.log(sortedKeys);
