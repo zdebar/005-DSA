d8d88bdc037c0b4ee44ad4f878f39c2a9cce8101
zdebar <157598603+zdebar@users.noreply.github.com>
2024-11-17 11:50:20 +0100

    practice

--- Javascript/W3/test.js
+++ Javascript/W3/test.js
@@ -1,24 +1,20 @@
-class Node {
-  constructor(value) {
-  this.value = value;
-  this.next = null;
-  }
-}
+let myArray = [64, 34, 25, 12, 22, 11, 90, 5];
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-  }
-
-  toString() {
-    let tempNode = this.head;
-    let result = "";
-    while (tempNode !== null) {
-      result += tempNode.value; 
-      
+function bubbleSort(arr) {
+  let sortedArray = [...arr];
+  let n = sortedArray.lenght;
+  for (let i = 0; i < n - i; i++ ) {
+    swapped = false;
+    for (let j = 0; j < n - i -1; j++) {
+      if (sortedArray[j] > sortedArray[j + 1]) {
+        [sortedArray[j], sortedArray[j+1]] =  [sortedArray[j+1], sortedArray[j]];
+        swapped = true;
+      } 
     }
-    return result;
+    if (!swapped) break;
   }
+  return sortedArray;
 }
+
+console.log(bubbleSort(myArray));
   
