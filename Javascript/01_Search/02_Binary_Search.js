4a960bd753056c1be16dd41525810f89ec3c4d13
zdebar <157598603+zdebar@users.noreply.github.com>
2024-11-24 10:05:45 +0100

    Factorial Algorithm

--- Javascript/01_Search/02_Binary_Search.js
+++ Javascript/01_Search/02_Binary_Search.js
@@ -2,12 +2,8 @@ function binarySearch(arr, targetVal) {
   let left = 0, right = arr.length - 1;
 
   while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
-
-
-    if (arr[mid] === targetVal) {
-      return mid; 
-    }
+    mid = left + Math.floor((right - left) / 2);
+    if (arr[mid] === targetVal) return mid; 
     if (arr[mid] < targetVal) {
       left = mid + 1;
     } else {
--- Javascript/02_Algorithms/01_Factorial.js
+++ Javascript/02_Algorithms/01_Factorial.js
@@ -1,4 +1,32 @@
+function factorial(n) {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+function factorialTabulation(n) {
+  let factorials =  [1]
+  for (let i = 1; i <= n; i++) {
+    factorials[i] = factorials[i - 1] * i;
+  }
+  return factorials[n];
+}
+
+function factorialRecursiveMemo(n, memo = []) {
+  if (n === 0 || n === 1) return 1;
+  if (n in memo) return memo[n];
+  memo[n] = n * factorialRecursiveMemo(n - 1, memo);
+  return memo[n]
+}
+
 function factorialRecursive(n) {
-  if (n === 1) return 1;
+  if (n === 0 || n === 1) return 1;
   return n * factorialRecursive(n - 1);
+}
+
+function factorialTailRecursive(n, accumulator = 1) {
+  if (n === 0 || n === 1) return accumulator;
+  return factorialTailRecursive(n - 1, n * accumulator); // Recursive call is the last operation
 }
