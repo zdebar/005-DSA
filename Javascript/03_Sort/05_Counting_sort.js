ef7fa672b233175d328d2170b386ee773c8904c0
zdebar <157598603+zdebar@users.noreply.github.com>
2024-12-03 13:02:03 +0100

    counting Sort

--- Javascript/03_Sort/05_Counting_sort.js
+++ Javascript/03_Sort/05_Counting_sort.js
@@ -1,18 +1,15 @@
 function countingSort2(array) {
     // counting object
-    let max = Math.max(...array);
     let count = {};
 
-    for (let num of array) {
+    for (const num of array) {
         count[num] = (count[num] || 0) + 1;
     }
 
     let index = 0;
-    for (let num in count) {
+    for (const num in count) {
         while (count[num]-- > 0) {
             array[index++] = Number(num);
         }        
     }
-
-    return array;
 }
