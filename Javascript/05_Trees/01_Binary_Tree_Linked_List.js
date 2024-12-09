90e7d78ea03cebc6791326d3a5622501a44346f4
zdebar <157598603+zdebar@users.noreply.github.com>
2024-12-09 14:58:32 +0100

    Binary trees

--- Javascript/05_Trees/01_Binary_Tree_Linked_List.js
+++ Javascript/05_Trees/01_Binary_Tree_Linked_List.js
@@ -10,38 +10,37 @@ class BinaryTree {
   constructor() {
       this.root = null;
   }
-}
 
-function inOrder(node) {
-  if (node === null) return;
-  inOrder(node.left); 
-  console.log(node.value); 
-  inOrder(node.right); 
-}
+  inOrder(node) {
+    if (node === null) return;
+    inOrder(node.left); 
+    console.log(node.value); 
+    inOrder(node.right); 
+  }
 
-function preOrder(node) {
-  if (node === null) return;
-  console.log(node.value); 
-  preOrder(node.left); 
-  preOrder(node.right); /
-}
+  preOrder(node) {
+    if (node === null) return;
+    console.log(node.value); 
+    preOrder(node.left); 
+    preOrder(node.right); 
+  }
 
-function postOrder(node) {
-  if (node === null) return;
-  postOrder(node.left); 
-  postOrder(node.right); 
-  console.log(node.value); 
-}
+  postOrder(node) {
+    if (node === null) return;
+    postOrder(node.left); 
+    postOrder(node.right); 
+    console.log(node.value); 
+  }
 
-function levelOrder(root) {
-  if (root === null) return;
-  const queue = [root]; 
-  while (queue.length > 0) {
-      const current = queue.shift(); 
-      console.log(current.value); 
+  levelOrder(root) {
+    if (root === null) return;
+    const queue = [root]; 
+    while (queue.length > 0) {
+        const current = queue.shift(); 
+        console.log(current.value); 
 
-      if (current.left !== null) queue.push(current.left); 
-      if (current.right !== null) queue.push(current.right); 
+        if (current.left !== null) queue.push(current.left); 
+        if (current.right !== null) queue.push(current.right); 
+    }
   }
 }
-
