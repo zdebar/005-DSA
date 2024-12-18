4adad5fde8cde4ab3bae10d4546b5ce62bbb8a54
zdebar <157598603+zdebar@users.noreply.github.com>
2024-12-18 12:52:32 +0100

    LeetCode 1

--- Javascript/LeetCode_75/1456.js
+++ Javascript/LeetCode_75/1456.js
@@ -3,10 +3,12 @@ var maxVowels = function(s, k) {
     let max = 0;
     let sum = 0;
 
+    // Sum first k item of array
     for (let i = 0; i < k; i++) {
         if (vowels.has(s[i])) sum++;
     }
 
+    // Sliding window
     max = sum;
     for (let i = k; i < s.length; i++) {
         if (vowels.has(s[i])) sum++;
