05fb2c3f09da41734dc0569ae7bfee8094c8b9cf
zdebar <157598603+zdebar@users.noreply.github.com>
2024-12-20 15:38:00 +0100

    LeetCode 01

---
+++ Javascript/LeetCode_75/933.js
@@ -0,0 +1,16 @@
+// Number of Recent Calls
+class RecentCounter {
+    constructor() {
+        this.q = [];
+    }
+    
+    ping(t) {
+        this.q.push(t);
+        while (this.q[0] < t - 3000) {
+            this.q.shift();
+        }
+        return this.q.length;
+
+    }
+}
+
